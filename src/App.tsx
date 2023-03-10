b79760e9e135cf1848940f5a46f3bdea4e8f8fd0
shobhit <shobhitmishra2002@gmail.com>
2023-03-10 16:34:16 +0530

    first commit and added navigation bar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="container default-font">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
