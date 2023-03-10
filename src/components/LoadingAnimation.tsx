a90b5e29165093539a3c4f5548414b7e4788e12f
shobhit <shobhitmishra2002@gmail.com>
2023-03-10 17:03:10 +0530

    added animation on reveal

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useState } from "react";
+import "../styles/LoadingAnimation.css";
+
+export const ExpandOnLoading: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [compStatus, setCompStatus] = useState<boolean>(false);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      setCompStatus(true);
+    }
+  }, [inView]);
+
+  return (
+    <div
+      className={`basic-styles ${compStatus ? "expand-reveal" : "expand-hide"}`}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+};
